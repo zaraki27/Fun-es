cf2a81b7d2a8918eaf65bf578450b9cd6af6394a
zaraki27 <108496481+zaraki27@users.noreply.github.com>
2024-07-02 17:10:33 -0300

    Teste de js

---
+++ script.js
@@ -0,0 +1,9 @@
+/*criande função*/
+function calculaMedia(nome, nota1, nota2, nota3){
+    const media = (nota1, nota2, nota3)/3;
+    console.log(nome+"Sua média é "+media)
+}
+
+/*chamadas da função*/
+calculaMedia("João", 30, 23, 15);
+calculaMedia("Pedro", 80, 52, 65);
